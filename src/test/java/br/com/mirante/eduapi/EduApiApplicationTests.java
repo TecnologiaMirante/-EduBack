684b6595f0bba6e2e6dda23281f87a55ec63b805
Arão Farias <arao.alves7@gmail.com>
2024-06-06 11:04:51 -0300

    Iniciando projeto

---
+++ src/test/java/br/com/mirante/eduapi/EduApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mirante.eduapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
