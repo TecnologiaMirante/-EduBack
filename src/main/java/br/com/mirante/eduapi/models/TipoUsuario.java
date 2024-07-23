76c0f3292c6146e978b37fd11dac128c90730aea
Martins98725 <icaro25martins@gmail.com>
2024-07-23 11:00:09 -0300

    adicionando tipo de usuario

---
+++ src/main/java/br/com/mirante/eduapi/models/TipoUsuario.java
@@ -0,0 +1,26 @@
+package br.com.mirante.eduapi.models;
+
+public enum TipoUsuario {
+    nome,
+    email,
+    telefone,
+    sexo,
+    cpf,
+    matricula,
+    avatar,
+    dataDeNascimento,
+    status,
+    instagram,
+    facebook,
+    whatsApp,
+    twitter,
+    estado,
+    cidade,
+    bairro,
+    numeroEndereco,
+    complemento,
+    cep,
+    referencia,
+    logradouro,
+
+}
