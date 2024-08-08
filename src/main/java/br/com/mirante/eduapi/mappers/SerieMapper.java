6263ee0a601a2f026a4ea8efd24e48c909f800ed
Guilherme Rosa <guilhermerosa1.br@gmail.com>
2024-08-08 20:02:50 -0300

    Nada

--- src/main/java/br/com/mirante/eduapi/mappers/SerieMapper.java
+++ src/main/java/br/com/mirante/eduapi/mappers/SerieMapper.java
@@ -2,7 +2,8 @@ package br.com.mirante.eduapi.mappers;
 
 import br.com.mirante.eduapi.dto.SerieDTO;
 import br.com.mirante.eduapi.models.Serie;
-import org.mapstruct.Mapper;
+import br.com.mirante.eduapi.service.SerieService;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
