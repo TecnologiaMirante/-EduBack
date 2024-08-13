c399a5ca8245d204bdf42d77cee508acfe1ba776
Gabriel costta <gabrielcostta210@gmail.com>
2024-08-12 23:58:08 -0300

    ajustando o DTO da questoes

--- src/main/java/br/com/mirante/eduapi/dto/QuestoesDTOGet.java
+++ src/main/java/br/com/mirante/eduapi/dto/QuestoesDTOGet.java
@@ -1,19 +1,15 @@
 package br.com.mirante.eduapi.dto;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 import java.util.UUID;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class OpcoesDTO {
+public class QuestoesDTOGet {
     private UUID id;
-    private String nota;
-    private Boolean correto;
-    private List<QuestoesDTOGet> questoesDTOS;
+    private String titulo;
+    private String descricao;
 }
--- src/main/java/br/com/mirante/eduapi/mappers/QuestoesMapper.java
+++ src/main/java/br/com/mirante/eduapi/mappers/QuestoesMapper.java
@@ -1,6 +1,7 @@
 package br.com.mirante.eduapi.mappers;
 
 import br.com.mirante.eduapi.dto.QuestoesDTO;
+import br.com.mirante.eduapi.dto.QuestoesDTOGet;
 import br.com.mirante.eduapi.models.Questoes;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -10,4 +11,6 @@ public interface QuestoesMapper {
     QuestoesMapper INSTANCE = Mappers.getMapper(QuestoesMapper.class);
     QuestoesDTO questoesToQuestoesDTO(Questoes questoes);
     Questoes questoesDTOToQuestoes(QuestoesDTO questoesDTO);
+
+    QuestoesDTOGet questoesToQuestoesDTOGet(Questoes questoes);
 }
