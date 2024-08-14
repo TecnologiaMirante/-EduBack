5e22fda0a33fb2a8fae6f0c53eef32fe1e0b322e
Martins98725 <icaro25martins@gmail.com>
2024-08-14 14:42:03 -0300

    fix QuestõesMapper

--- src/main/java/br/com/mirante/eduapi/mappers/OpcoesMapper.java
+++ src/main/java/br/com/mirante/eduapi/mappers/OpcoesMapper.java
@@ -9,13 +9,9 @@ import org.mapstruct.factory.Mappers;
 public interface OpcoesMapper {
     OpcoesMapper INSTANCE = Mappers.getMapper(OpcoesMapper.class);
 
-    @Mappings({
-            @Mapping(source = "questoes", target = "questoesDTOS")
-    })
+
     OpcoesDTO opcoestoOpcoesDTO(Opcoes opcoes);
 
-    @Mappings({
-            @Mapping(source = "questoesDTOS", target = "questoes")
-    })
+
     Opcoes opcoesDTOtoOpcoes(OpcoesDTO opcoesDTO);
 }
